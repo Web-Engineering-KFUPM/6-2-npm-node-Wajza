6b907c098094f5ba5bf5d6cf1f1f25db4a4f1960
Wajza <s202262140@kfupm.edu.sa>
2026-04-01 11:45:44 +0300

    Debug

--- utils/parser.js
+++ utils/parser.js
@@ -2,7 +2,8 @@ import _ from "lodash";
 
 export function parseNumbers(input) {
   const numbers = _.map(input, (str) => Number(str));
-  return _.compact(numbers);
+  // Keep valid numbers (including 0), drop NaN only
+  return _.filter(numbers, (num) => !Number.isNaN(num));
 }
 
 export function isValidOperation(operation) {
