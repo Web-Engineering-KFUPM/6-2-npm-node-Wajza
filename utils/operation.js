48f47d45f4ddab979b5592a2babb10a2963bc78e
Wajza <s202262140@kfupm.edu.sa>
2026-04-01 11:19:39 +0300

    TODO4

--- utils/operation.js
+++ utils/operation.js
@@ -1,14 +1,22 @@
-  export function add(numbers) {
-    return {}
-    }
-    export function subtract(numbers) {
-      return {}
-    }
+export function add(numbers) {
+  return numbers.reduce((sum, num) => sum + num, 0);
+}
 
-    export function multiply(numbers) {
-      return {}
-    }
+export function subtract(numbers) {
+  return numbers.reduce((result, num, index) => {
+    if (index === 0) return num;
+    return result - num;
+  });
+}
 
-    export function divide(numbers) {
-      return {}
-    }
+export function multiply(numbers) {
+  return numbers.reduce((product, num) => product * num, 1);
+}
+
+export function divide(numbers) {
+  return numbers.reduce((result, num, index) => {
+    if (index === 0) return num;
+    if (num === 0) return NaN;
+    return result / num;
+  });
+}
